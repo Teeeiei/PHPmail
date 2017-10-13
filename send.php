0746e08f439a19de771aef682be16fb6ff8fcbe6
Teeeiei <32706196+Teeeiei@users.noreply.github.com>
2017-10-13 19:07:44 +0700

    Add files via upload

---
+++ send.php
@@ -0,0 +1,28 @@
+<?php
+require_once('class.phpmailer.php');
+$mail = new PHPMailer();
+$mail->IsHTML(true);
+$mail->IsSMTP();
+$mail->SMTPAuth = true; // enable SMTP authentication
+$mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+$mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+$mail->Port = 465; // set the SMTP port for the GMAIL server
+$mail->Username = "pete199599@gmail.com"; // GMAIL username
+$mail->Password = "peet991995"; // GMAIL password
+$mail->From = "piranon.n@gmail.com"; // "name@yourdomain.com";
+$mail->FromName = "piranon.com";  // set from Name
+$mail->Subject = "สวัสดีครับ ทดสอบการส่งเมล์ครับ"; 
+$mail->Body = 'ทดสอบการส่งเมล์ครับ body ครับ';
+ 
+$mail->AddAddress('kanawat320@gmail.com'); // to Address
+ 
+$mail->set('X-Priority', '3'); //Priority 1 = High, 3 = Normal, 5 = low
+if(!$mail->Send()) 
+{
+    echo 'Mailer Error: ' . $mail->ErrorInfo.'<br />';
+} 
+else 
+{
+    echo 'Message has been sent<br />';
+}
+?>
